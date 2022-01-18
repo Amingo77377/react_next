6e825ca79560e0962dc0c7a1793e61055871c5ba
MingLee <Ming_lee@wiedu.com>
2022-01-18 20:28:34 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
+  )
+}
+
+export default HomePage
