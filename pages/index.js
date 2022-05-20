9d04a87ff060edd2e6ca076c66859ec058ceb3e0
Amingo77377 <45022874+Amingo77377@users.noreply.github.com>
2022-05-20 17:49:53 +0800

    freshDesk
    test

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,19 @@ function HomePage() {
       <li>2</li>
       <li>3</li>
     </ul>
+    <script>
+    console.log('freshDesk')
+    window.fwSettings={
+    'widget_id':67000003397
+    };
+    !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}() 
+  </script>
+  <script type='text/javascript' src='https://widget.freshworks.com/widgets/67000003397.js' async defer></script>
+  <script type='text/javascript'>
+    // FreshworksWidget('show', 'launcher');
+    FreshworksWidget('open');
+
+  </script>
   </div>
   )
 }
