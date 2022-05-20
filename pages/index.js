75e51dfc7b8e90ffcfa182b03bf0518fe6418894
Amingo77377 <45022874+Amingo77377@users.noreply.github.com>
2022-05-20 17:54:32 +0800

    test 2
    test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
 function HomePage() {
+  useEffect(() => {
+  const script = document.createElement('script');
+     window.fwSettings={
+      'widget_id':67000003397
+      };
+      !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}() 
+    script.src = "https://widget.freshworks.com/widgets/67000003397.js";
+    script.type = 'text/javascript'
+    script.defer = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+  
   return (
     <div>
       <h1>Welcome to Next.js!</h1>
@@ -7,19 +24,6 @@ function HomePage() {
       <li>2</li>
       <li>3</li>
     </ul>
-    <script>
-    console.log('freshDesk')
-    window.fwSettings={
-    'widget_id':67000003397
-    };
-    !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}() 
-  </script>
-  <script type='text/javascript' src='https://widget.freshworks.com/widgets/67000003397.js' async defer></script>
-  <script type='text/javascript'>
-    // FreshworksWidget('show', 'launcher');
-    FreshworksWidget('open');
-
-  </script>
   </div>
   )
 }
